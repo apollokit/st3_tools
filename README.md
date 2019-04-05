6edc61b1c7e3e429231f504ae878130fbec506ee
Kit Kennedy <kit@vinci.co>
2019-04-05 15:35:42 -0700

    Adding new command for multiple cursors from a comma separated list. Also adding commands in tools.py to the command palette

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # st3_tools
 Various tools &amp; commands for Sublime Text 3
 
+See the API at https://www.sublimetext.com/docs/3/api_reference.html
+
+Also see https://www.1klb.com/posts/2013/07/18/building-a-sublimetext-command-from-scratch-and-other-useful-bits/ for some helpful context
+
 ## Manual Installation
 
 - `cd <Packages directory>`   (for example on Mac it is `~/Library/Application\ Support/Sublime\ Text\ 2/Packages` or `~/Library/Application\ Support/Sublime\ Text\ 3/Packages`)
--- tools.py
+++ tools.py
@@ -1,24 +1,79 @@
+"""Rando tools for sublime text
+
+Author: Kit Kennedy
+"""
+
 import sublime, sublime_plugin
 
 class CursorsFromSelectionCommand(sublime_plugin.TextCommand):
     """ For a block(s) of selected text, unselect and add a cursor at the beginning of every line
+
+    The command name for key-bindings etc will be cursors_from_selection
     """
     def run(self, edit):
 
+        print('Run CursorsFromSelection')
         # view.line(view.sel()[0])
 
         new_regions = []
         for region in self.view.sel():
             line_begs = [line_regs.begin() for line_regs in self.view.lines(region)]
-            new_regions += [sublime.Region(line_beg,line_beg) for line_beg in line_begs]
+            new_regions += [sublime.Region(line_beg, line_beg) for line_beg in line_begs]
 
         self.view.sel().clear()
         for reg in new_regions:
             self.view.sel().add(reg)
 
-        #     line_contents = self.view.substr(line)
+class CursorsFromCommaListCommand(sublime_plugin.TextCommand):
+    """ For a block(s) of selected text, unselect and add a cursor before the first
+    non-whitespace character after every comma
 
-        #     if up:
-        #       self.view.insert(edit, line.end(), "\n" + line_contents)
-        #     else:
-        #       self.view.insert(edit, line.begin(), line_contents + "\n")
+    The command name for key-bindings etc will be cursors_from_comma_list
+    """
+    def run(self, edit):
+        print('Run CursorsFromCommaList')
+        new_regions = []
+        # for each selection region
+        for region in self.view.sel():
+            # get the text from the region
+            the_text = self.view.substr(region)
+            the_text_begins = region.begin()
+
+            def comma_parse(dat_text, comma_parse_state='gobbling_whitespace'):
+                """Parse a string, returning indices immediately following a comma+whitespace)
+
+                start out pretending we just found a command right before the text, so that we'll
+                add the beginning of the string indx as well
+                                
+                Args:
+                    dat_text: the text
+                
+                Returns:
+                    List of indices within the string
+                    List[int]
+                """
+                indcs = []
+                for indx, char in enumerate(dat_text):
+                    if comma_parse_state == 'searching_comma':
+                        if char == ',':
+                            comma_parse_state = 'gobbling_whitespace'
+                        else: pass
+                    elif comma_parse_state == 'gobbling_whitespace':
+                        if char == ' ': pass
+                        else: 
+                            indcs.append(indx)
+                            comma_parse_state = 'searching_comma'
+                return indcs
+
+            # find the indices of all commas in the text            
+            comma_indcs = comma_parse(the_text)
+            comma_indcs = [indx + the_text_begins for indx in comma_indcs]
+            # make a new region for each post-comma point. The region is empty because all we want
+            # to do is add a cursor at the beginning
+            new_regions += [sublime.Region(indx, indx) for indx in comma_indcs]
+
+        # clear cursors
+        self.view.sel().clear()
+        # add a cursors at each of the discovered regions
+        for reg in new_regions:
+            self.view.sel().add(reg)
